0b4bcdc3da8ac97c32313abae266dc9a8a33ca9a
Kim-jaeyeon <jy020507@naver.com>
2024-05-27 00:03:15 +0900

    [Fix] #5 -fix the wide screen ver
    와이드 화면일 때 발생하는 display 오류를 수정 하였다.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var bottom=document.getElementById("bottom")
 var menuSelected=false;
 
 
-links[0].addEventListener("click",()=>{
+links[0].addEventListener("click",()=>{     //각 메뉴를 눌렀을 때 페이지 화면 변환과 홈버튼 변환
     menuSelected=true;
     document.querySelector("#home_btn p").innerText="🏠";
     for(var i=0;i<pages.length;i++){
@@ -83,7 +83,7 @@ links[4].addEventListener("click",()=>{
     }
 
 });
-homeBtn.addEventListener("click",(event)=>{
+homeBtn.addEventListener("click",()=>{     //홈버튼 클릭시 초기상태로 변환
     menuSelected=false;
     for(var i=0;i<pages.length;i++) pages[i].style.display='';
     if(!menuSelected){
@@ -98,45 +98,94 @@ pages[0].style.animation="fadein 1s ease-in"
 
 window.addEventListener("scroll",()=>{
     var value=window.scrollY;
-    console.log(value,menuSelected);
+    
     if(menuSelected!=true){
-        if(value<=600){
-            pages[0].style.animation="fadein 1s ease-in"
-            pages[0].style.opacity="1";
-            pages[1].style.opacity="0";
-            pages[1].style.transition="1s ease-out"
+        if(window.matchMedia("(min-width: 1920px)").matches){       //와이드 화면일 때!!!!
+            
+            if(value<=600){
+                pages[0].style.animation="fadein 1s ease-in"
+                pages[0].style.opacity="1";
+                pages[1].style.opacity="0";
+                pages[1].style.transition="1s ease-out"
+            }
+            else if(value>600&&value<=1200){
+                pages[1].style.animation="fadein 1s ease-in"
+                pages[1].style.opacity="1";
+                pages[0].style.opacity="0";
+                pages[0].style.transition="1s ease-out"
+                pages[2].style.opacity="0";
+                pages[2].style.transition="1s ease-out"
+            }
+            else if(value>1200&&value<=2000){
+                pages[2].style.animation="fadein 1s ease-in"
+                pages[2].style.opacity="1";
+                pages[1].style.opacity="0";
+                pages[1].style.transition="1s ease-out"
+                pages[3].style.opacity="0";
+                pages[3].style.transition="1s ease-out"
+            }
+            else if(value>2000&&value<=2800){
+                pages[3].style.animation="fadein 1s ease-in"
+                pages[3].style.opacity="1";
+                pages[2].style.opacity="0";
+                pages[2].style.transition="1s ease-out"
+                pages[4].style.opacity="0";
+                pages[4].style.transition="1s ease-out"
+            }
+            else if(value>2800&&value<=4000){
+                pages[4].style.animation="fadein 1s ease-in"
+                pages[4].style.opacity="1";
+                pages[3].style.opacity="0";
+                pages[3].style.transition="1s ease-out"
+               
+            }
         }
-        else if(value>600&&value<=1400){
-            pages[1].style.animation="fadein 1s ease-in"
-            pages[1].style.opacity="1";
-            pages[0].style.opacity="0";
-            pages[0].style.transition="1s ease-out"
-            pages[2].style.opacity="0";
-            pages[2].style.transition="1s ease-out"
+        else{   //와이드 화면과 스마트폰 화면일 때
+            if(value<=600){
+                pages[0].style.animation="fadein 1s ease-in"
+                pages[0].style.opacity="1";
+                pages[1].style.opacity="0";
+                pages[1].style.transition="1s ease-out"
+            }
+            else if(value>600&&value<=1400){
+                pages[1].style.animation="fadein 1s ease-in"
+                pages[1].style.opacity="1";
+                pages[0].style.opacity="0";
+                pages[0].style.transition="1s ease-out"
+                pages[2].style.opacity="0";
+                pages[2].style.transition="1s ease-out"
+            }
+            else if(value>1400&&value<=2000){
+                pages[2].style.animation="fadein 1s ease-in"
+                pages[2].style.opacity="1";
+                pages[1].style.opacity="0";
+                pages[1].style.transition="1s ease-out"
+                pages[3].style.opacity="0";
+                pages[3].style.transition="1s ease-out"
+            }
+            else if(value>2000&&value<=2600){
+                pages[3].style.animation="fadein 1s ease-in"
+                pages[3].style.opacity="1";
+                pages[2].style.opacity="0";
+                pages[2].style.transition="1s ease-out"
+                pages[4].style.opacity="0";
+                pages[4].style.transition="1s ease-out"
+            }
+            else if(value>2600&&value<=4000){
+                pages[4].style.animation="fadein 1s ease-in"
+                pages[4].style.opacity="1";
+                pages[3].style.opacity="0";
+                pages[3].style.transition="1s ease-out"
+               
+            }
         }
-        else if(value>1400&&value<=2000){
-            pages[2].style.animation="fadein 1s ease-in"
-            pages[2].style.opacity="1";
-            pages[1].style.opacity="0";
-            pages[1].style.transition="1s ease-out"
-            pages[3].style.opacity="0";
-            pages[3].style.transition="1s ease-out"
-        }
-        else if(value>2000&&value<=2600){
-            pages[3].style.animation="fadein 1s ease-in"
-            pages[3].style.opacity="1";
-            pages[2].style.opacity="0";
-            pages[2].style.transition="1s ease-out"
-            pages[4].style.opacity="0";
-            pages[4].style.transition="1s ease-out"
-        }
-        else if(value>2600&&value<=4000){
-            pages[4].style.animation="fadein 1s ease-in"
-            pages[4].style.opacity="1";
-            pages[3].style.opacity="0";
-            pages[3].style.transition="1s ease-out"
-           
-        }
+
+
+
+
+
+
+        
     }else{
         pages[0].style.opacity="1";
         pages[1].style.opacity="1";
@@ -157,21 +206,5 @@ window.addEventListener("scroll",()=>{
     
     
     
-    // if(value<600){
-    //     pages[0].style.animation="fadein 1s ease-in";
-    //     pages[1].style.animation="fadeout 1s ease-out";
-    //     pages[1].style.opacity="0"
-    // }
-    // else if(value>=200&&value<600){
-    //     pages[1].style.animation="fadein 1s ease-in";
-        
-    // }
-    // else if(value>=600&&value<1400){
-    //     pages[0].style.animation="fadeout 1s ease-out"
-    //     pages[0].style.opacity="0"
-    // }
-    // else if(value>=1400&&value<2200){
-    //     pages[2].style.animation="fadein 1s ease-in";
-    //     pages[1].style.animation="fadeout 1s ease-out"
-    // }
+   
 })
